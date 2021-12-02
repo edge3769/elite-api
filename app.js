6999d40a72688a76ac0b149dc9627a255262c9d1
therealJimoh <jimoh.abdullah69@gmail.com>
2021-12-02 14:48:38 +0100

    resolve conflict

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
-      "http://localhost:3002",
-      "http://localhost:3306",
+      "http://localhost:3002"
     ],
     credentials: true,
   })
